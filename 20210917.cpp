dd53608a4a3452f6f60feaed1061a7f9626d1f4b
KANGYOOHA <wouiomk@daum.net>
2021-09-17 12:36:43 +0900

    오늘 처음

---
+++ 20210917.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int MyCash = 2344546;
+	float HP = 100.0f;
+	char H = '\q';  // 특수문자 외우기 \n(줄바꾸기),\t,\b 
+
+	cout << "aaa" << H;
+
+	return 0;
+}
