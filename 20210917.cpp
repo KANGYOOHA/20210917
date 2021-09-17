4fb70197cdea3c6986660128768c906737b3c987
KANGYOOHA <wouiomk@daum.net>
2021-09-17 12:52:14 +0900

    수정

--- 20210917.cpp
+++ 20210917.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 int main() {
 
-	int MyCash = 2344546;
-	float HP = 100.0f;
-	char H = '\q';  // 특수문자 외우기 \n(줄바꾸기),\t,\b 
+	int MyCash = 0;
+	//float HP = 100.0f;
+	//char H = '\n';  // 특수문자 외우기 \n(줄바꾸기),\t,\b 
 
-	cout << "aaa" << H;
+	//키보드 입력 받아서 MyCash에 저장
+	
+	cin >> MyCash;
+
+	//MyCash 내용을 모니터
+	cout << MyCash;
 
 	return 0;
 }
